da13293fdaf932a1a444d0aa29c0fa8f4aad38f4
Blieper <yoranmandema@eml.cc>
2019-11-29 19:56:04 +0100

    Add skrypt importer

---
+++ Assets/Scripts/Skrypt/SkryptTypeConverter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Skrypt;
+
+public static class SkryptTypeConverter {
+    public static Vector3 FromSkryptVector3 (Vector3Instance vector3Instance) {
+        return new Vector3(
+            (float)vector3Instance.Components[0],
+            (float)vector3Instance.Components[1],
+            (float)vector3Instance.Components[2]
+            );
+    }
+
+}
