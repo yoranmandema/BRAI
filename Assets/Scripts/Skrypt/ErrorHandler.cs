79f867025e2fe2bf94b5a270de7e51df77738cab
Blieper <yoranmandema@eml.cc>
2019-11-29 20:45:10 +0100

    ErrorHandler

---
+++ Assets/Scripts/Skrypt/ErrorHandler.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using Skrypt;
+
+public class ErrorHandler : Skrypt.ErrorHandler {
+
+    public ErrorHandler(SkryptEngine engine) : base(engine) {
+    }
+
+    public override void FatalError(int index, int line, int column, string msg) {
+        throw CreateError(index,line,column,msg);
+    }
+
+    public override string ReportError(SkryptException error) {
+        Debug.LogError(error);
+
+        return error.ToString();
+    }
+}
